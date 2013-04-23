7eb7dd6d15b1e251cef68f3a54f9873c8c473ecb
andreas <encrypt.wave@gmail.com>
2013-04-23 22:51:33 +0200

    Revert "dummy data"
    This reverts commit 1fe43afab1e18586b3978b817e9ff07f21634810.

--- client/game/game.js
+++ client/game/game.js
@@ -5,37 +5,5 @@ function Game(map, config, ui) {
 }
 
 Game.prototype.step = function(deltatime) {
-		var samplestate = {	// sample state
-		bullets: [
-			{
-				id: 0,
-				owning_player: 0,
-				current_position: {
-					x: 100,
-					y: 100,
-				},
-				direction: {
-					x: 1,
-					y: 1,
-				}
-			},
-		],
-		units: [
-			{
-				id: 0,
-				owning_player: 0,
-				current_position: {
-					x: 100,
-					y: 100
-				},
-				target_position: {
-					x: 100,
-					y: 100
-				},
-				shooting_cooldown: 0
-			},
-		],
-	}
-	
-	this.ui.render(deltatime, samplestate)
+	ui.render(deltatime, {/* state */})
 }
