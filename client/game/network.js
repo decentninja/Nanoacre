9cdc0ad72cb6e6e06e9e7672e6c6770776dd13ce
Viktor Palmkvist <viktor@palmkvist.com>
2013-04-24 12:23:04 +0200

    network.ready should no longer expose this in the callback function

--- client/game/network.js
+++ client/game/network.js
@@ -8,63 +8,67 @@ function Network(websocket, eventqueue, pongCount) {
 	websocket.onerror = this.onerror
 }
 
-Network.prototype.send = function(message) {
-	if (typeof message == "string" || message instanceof String) {
-		websocket.send(message)
-	} else {
-		websocket.send(JSON.stringify(message))
-	}
-}
+function() {
+	var startFunc
 
-Network.prototype.ready = function(startFunc) {
-	this.startFunc = startFunc //TODO: what is 'this' bound to when calling startFunc?
-	this.latency = new Array()
-	this.send("ready")
-}
-
-Network.prototype.onopen = function(e) {}
-
-Network.prototype.onmessage = function(e) {
-	var message = e.data
-	switch (message) {
-		case "ping":
-			this.gotPing()
-			break;
-
-		case "start":
-			this.startFunc(this.latency)
-			break;
-
-		default:
-			eventqueue.push(JSON.parse(message))
-	}
-}
-
-Network.prototype.onclose = function(e) {}
-
-Network.prototype.onerror = function(e) {}
-
-Notwork.prototype.gotPing = function() {
-	now = performance.now()
-	var done = this.latency.length + 1 >= pongCount
-	if (!done) {
-		this.send("pong")
-	} else {
-		this.send("lastpong")
+	Network.prototype.send = function(message) {
+		if (typeof message == "string" || message instanceof String) {
+			websocket.send(message)
+		} else {
+			websocket.send(JSON.stringify(message))
+		}
 	}
 
-	if (this.lastPing)
-		this.latency.push(now - this.lastPing)
-	this.lastPing = now
-
-	if (done) {
-		var avgLat = 0
-		this.latency.forEach(function(lat) {
-			avgLat += lat
-		})
-		avgLat /= this.latency.length
-		this.latency = avgLat
-		delete this.lastPing
+	Network.prototype.ready = function(startFunction) {
+		startFunc = startFunction
+		this.latency = new Array()
+		this.send("ready")
 	}
-}
 
+	Network.prototype.onopen = function(e) {}
+
+	Network.prototype.onmessage = function(e) {
+		var message = e.data
+		switch (message) {
+			case "ping":
+				this.gotPing()
+				break;
+
+			case "start":
+				startFunc(this.latency)
+				break;
+
+			default:
+				eventqueue.push(JSON.parse(message))
+		}
+	}
+	
+	Network.prototype.onclose = function(e) {}
+	
+	Network.prototype.onerror = function(e) {}
+
+	Notwork.prototype.gotPing = function() {
+		now = performance.now()
+		var done = this.latency.length + 1 >= pongCount
+		if (!done) {
+			this.send("pong")
+		} else {
+			this.send("lastpong")
+		}
+
+		if (this.lastPing)
+			this.latency.push(now - this.lastPing)
+		this.lastPing = now
+
+		if (done) {
+			var avgLat = 0
+			this.latency.forEach(function(lat) {
+				avgLat += lat
+			})
+			avgLat /= this.latency.length
+			this.latency = avgLat
+			delete this.lastPing
+		}
+	}
+
+}()
