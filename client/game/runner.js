edfd6735c12819c0d87f808e9ee1fb1562334feb
Simon Lindholm <simon.lindholm10@gmail.com>
2013-04-24 13:36:29 +0200

    Add a timeline, with some prototype logic

--- client/game/runner.js
+++ client/game/runner.js
@@ -1,9 +1,7 @@
-function Runner(socket, canvas_selector, config) {
-	var canvas_element = document.querySelector(canvas_selector)
-	var canvas_context = canvas_element.getContext('2d')
-	if(config.fullscreen) {
-		// DO fullscreen
-	}
+function Runner(socket, canvas, config) {
+	this.canvas = canvas
+	var canvas_context = canvas.getContext('2d')
+	this.config = config
 
 	var samplemap = {
 		name: "Valley of Darkness",
@@ -14,20 +12,36 @@ function Runner(socket, canvas_selector, config) {
 		],
 	}
 
-	this.eventqueue = new Array()
+	this.eventqueue = []
 
 	this.ui = new Ui(canvas_context, config, samplemap)
 	this.game = new Game(samplemap, config, this.ui)
 
 	this.network = new Network(socket, this.eventqueue, 10)
 
-	this.lasttime = performance.now()
-
 	//TODO: when the client is ready to start, network.ready should be called
 	//this.network.ready(startFunc)
 	//where startFunc is a function that takes the time since the server
 	//sent the start command and starts the gameloop with an adjusted
 	//gameclock
+}
+
+Runner.prototype.start = function() {
+	if(this.config.fullscreen) {
+		// DO fullscreen
+	}
+	var that = this
+	var canvas = this.canvas
+	this.canvas.onmousedown = function(ev) {
+		// If we use jQuery, this is just (ev.pageX - $(ev).offset().left), etc.
+		var docElem = document.documentElement
+		var bclr = that.canvas.getBoundingClientRect()
+		var x = ev.pageX - Math.round(bclr.left + window.pageXOffset - docElem.clientTop)
+		var y = ev.pageY - Math.round(bclr.top + window.pageYOffset - docElem.clientLeft)
+		that.ui.handleMousedown(x, y, that.game)
+	}
+	this.lasttime = performance.now()
+	this.loop();
 }
 
 Runner.prototype.loop = function() {
@@ -38,7 +52,7 @@ Runner.prototype.loop = function() {
 	requestAnimationFrame(this.loop.bind(this))
 }
 
-var runner = new Runner(null, ".canvas", {
+var runner = new Runner(null, document.querySelector(".canvas"), {
 	colors: {
 		teams: [
 			"#7EA885",
@@ -54,4 +68,4 @@ var runner = new Runner(null, ".canvas", {
 	fullscreen: true,
 })
 
-runner.loop()
+runner.start()
