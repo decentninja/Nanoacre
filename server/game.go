f1f9cbc07e349a35ef0b9542d9552ad083191558
Viktor Palmkvist <viktor@palmkvist.com>
2013-05-05 17:35:02 +0200

    Multiple game 'lobbies', serverside.

--- server/game.go
+++ server/game.go
@@ -1,5 +1,10 @@
 package server
 
+import (
+	"fmt"
+	"log"
+)
+
 const (
 	PING  = "ping"
 	START = "start"
@@ -7,19 +12,42 @@ const (
 )
 
 type game struct {
-	players []*player
-	ch      chan *message
+	id           int
+	players      []*player
+	ch           chan *message
+	parentCustom *custom
 }
 
 type loadData struct {
-	Id int
-	Field *playfield
+	Id      int
+	Options []string
+	Field   *playfield
 }
 
-func (g *game) load(field *playfield) {
-	for i, p := range g.players {
-		p.sendJSON(loadData{i, field})
+func (g *game) gatherPlayers() {
+	for p := range g.parentCustom.newPlayerChannel {
+		p.ch = g.ch
+		g.players = append(g.players, p)
+		log.Printf("Game %s has %d/%d players.\n", g.str(), len(g.players), g.parentCustom.numPlayers)
+		if len(g.players) == g.parentCustom.numPlayers {
+			break
+		}
 	}
+	g.parentCustom.spawnNewGame()
+	g.load()
+	g.run()
+}
+
+func (g *game) load() {
+	field := g.parentCustom.getField()
+	for i, p := range g.players {
+		p.sendJSON(loadData{
+			Id:      i,
+			Options: g.parentCustom.options,
+			Field:   field,
+		})
+	}
+	log.Printf("Game %s has sent loadData.\n", g.str())
 }
 
 func (g *game) run() {
@@ -64,6 +92,7 @@ func (g *game) startIfAllChecked() {
 	}
 
 	g.sendToAll(START)
+	log.Printf("Game %s has sent a start signal.\n", g.str())
 }
 
 func (g *game) sendToAllOthers(mess *message) {
@@ -78,4 +107,8 @@ func (g *game) sendToAll(data string) {
 	for _, p := range g.players {
 		p.send(data)
 	}
+}
+
+func (g *game) str() string {
+	return fmt.Sprintf("%s:%d", g.parentCustom.name, g.id)
 }
--- server/server.go
+++ server/server.go
@@ -4,43 +4,80 @@ import (
 	"code.google.com/p/go.net/websocket"
 
 	"log"
-	"net/http"
 	"math/rand"
+	"net/http"
 	"path/filepath"
 )
 
-var waitingConnection = make(chan *player)
-var fields []*playfield
+type server struct {
+	customs   map[string]*custom
+	allFields []*playfield
+}
+
+type custom struct {
+	name             string
+	nextId           int
+	newPlayerChannel chan *player
+	fields           []*playfield
+	options          []string
+	numPlayers       int
+}
 
 func SetupSocketServer(field string) {
-	http.Handle("/ws", websocket.Handler(newConnection))
+	var fields []*playfield
 	if field == "" {
 		fields = readFieldsFromFolder("maps")
 	} else {
 		fields = []*playfield{readFieldFromFile(filepath.Join("maps", field))}
 	}
+	l := &server{
+		customs:   make(map[string]*custom),
+		allFields: fields,
+	}
+	l.customs["default"] = l.newDefaultCustom("default")
+	handler := websocket.Handler(l.newConnection())
+	http.Handle("/ws", handler)
 }
 
-func newConnection(ws *websocket.Conn) {
-	log.Println("Got new connection.")
-	p := &player{
-		conn: ws,
-		ch:   make(chan *message),
-	}
-	select {
-	case waitingConnection <- p:
-		p.listen()
-
-	case otherP := <-waitingConnection:
-		p.ch = otherP.ch
-		go p.listen()
-
-		g := &game{
-			players: []*player{p, otherP},
-			ch:      p.ch,
+func (l *server) newConnection() func(*websocket.Conn) {
+	return func(ws *websocket.Conn) {
+		customKey := ws.Request().URL.Query().Get("custom")
+		c, exists := l.customs[customKey]
+		if !exists { //TODO: data race might lose one player if connections are simultaneous
+			l.customs[customKey] = l.newDefaultCustom(customKey)
 		}
-		log.Println("Starting a new game.")
-		g.load(fields[rand.Intn(len(fields))])
-		g.run()
+		log.Printf("Got new connection in custom: \"%s\"\n", customKey)
+		p := &player{
+			conn: ws,
+		}
+		c.newPlayerChannel <- p
+		p.listen()
 	}
+}
+
+func (l *server) newDefaultCustom(name string) *custom {
+	c := &custom{
+		name:             name,
+		fields:           l.allFields,
+		options:          make([]string, 0, 0),
+		newPlayerChannel: make(chan *player),
+	}
+	c.spawnNewGame()
+	return c
+}
+
+func (c *custom) spawnNewGame() {
+	g := &game{
+		players:      make([]*player, 0),
+		ch:           make(chan *message),
+		parentCustom: c,
+		id:           c.nextId,
+	}
+	c.nextId++
+	log.Printf("Spawning game %s.\n", g.str())
+	go g.gatherPlayers()
+}
+
+func (c *custom) getField() *playfield {
+	return c.fields[rand.Intn(len(c.fields))]
 }
